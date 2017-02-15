08c0d11004c35ebb1266cfbb5e72b13b5f4a3ee0
Tyler Shambora <tyler@bvaccel.com>
2017-02-14 20:44:18 -0800

    initial commit

---
+++ js/ads.js
@@ -0,0 +1,16 @@
+var Ads = [
+  {
+    text: "Rock out to the sounds of the Universe",
+    filename: "space-cd-ad.jpg"
+  },
+
+  {
+    text: "Extra-terrestial dating",
+    filename: "space-love-ad.jpg"
+  },
+
+  {
+    text: "Gourmet vacuum sealed meals.",
+    filename: "space-food-ad.jpg"
+  }
+]
---
+++ js/planets.js
@@ -0,0 +1,100 @@
+Planets = {
+  mercury: {
+    name: "Mercury",
+    videoId: "ozd9nPo_VAk",
+    price: 1000000,
+    distance: 80790000,
+    blurb: "Mercury is the smallest and closest to the Sun of the eight planets in the Solar System, with an orbital period of about 88 Earth days. Seen from the Earth, it appears to move around its orbit in about 116 days, which is much faster than any other planet. This rapid motion may have led to it being named after the Roman deity Mercury, the fast-flying messenger to the gods.",
+    wikipediaPage: "Mercury_(planet)"
+  },
+
+  mars: {
+    name: "Mars",
+    videoId: "Glah07CTS7c",
+    price: 2000000,
+    distance: 229200000,
+    blurb: "Mars is the fourth planet from the Sun and the second smallest planet in the Solar System. Named after the Roman god of war, it is often described as the 'Red Planet', as the iron oxide prevalent on its surface gives it a reddish appearance. Mars is a terrestrial planet with a thin atmosphere, having surface features reminiscent both of the impact craters of the Moon and the volcanoes, valleys, deserts, and polar ice caps of Earth.",
+    wikipediaPage: "Mars"
+  },
+
+  earth: {
+    name: "Earth",
+    videoId: "_jLGa4X5H2c",
+    price: 0,
+    distance: 0,
+    blurb: "Earth is the third planet from the Sun, and the densest and fifth-largest of the eight planets in the Solar System. It is also the largest of the Solar System's four terrestrial planets. It is sometimes referred to as the world or the Blue Planet. Earth formed approximately 4.54 billion years ago, and life appeared on its surface within its first billion years. Earth's biosphere then significantly altered the atmospheric and other basic physical conditions, which enabled the proliferation of organisms as well as the formation of the ozone layer, which together with Earth's magnetic field blocked harmful solar radiation, and permitted formerly ocean-confined life to move safely to land.",
+    wikipediaPage: "Earth"
+  },
+
+  venus: {
+    name: "Venus",
+    videoId: "TR-IWhZZDJY",
+    price: 3000000,
+    distance: 148200000,
+    blurb: "Venus is the second planet from the Sun, orbiting it every 224.7 Earth days. Because Venus is an inferior planet from Earth, it never appears to venture far from the Sun: its elongation reaches a maximum of 47.8°.",
+    wikipediaPage: "Venus"
+  },
+
+  saturn: {
+    name: "Saturn",
+    videoId: "MS9D9CzwPQU",
+    price: 4000000,
+    distance: 843900000,
+    blurb: "Saturn is the sixth planet from the Sun and the second largest planet in the Solar System, after Jupiter. Named after the Roman god of agriculture, Saturn, its astronomical symbol represents the god's sickle. Saturn is a gas giant with an average radius about nine times that of Earth. While only one-eighth the average density of Earth, with its larger volume Saturn is just over 95 times more massive than Earth.",
+    wikipediaPage: "Saturn"
+  },
+
+  jupiter: {
+    name: "Jupiter",
+    videoId: "bhSG_A_-CPE",
+    price: 5000000,
+    distance: 569300000,
+    blurb: "Jupiter is the fifth planet from the Sun and the largest planet in the Solar System. It is a gas giant with mass one-thousandth that of the Sun but is two and a half times the mass of all the other planets in the Solar System combined. Jupiter is classified as a gas giant along with Saturn, Uranus and Neptune. Together, these four planets are sometimes referred to as the Jovian or outer planets",
+    wikipediaPage: "Jupiter"
+  },
+
+  neptune: {
+    name: "Neptune",
+    videoId: "EmUvTH7sveg",
+    price: 6000000,
+    distance: 2763000000,
+    blurb: "Neptune is the eighth and farthest planet from the Sun in the Solar System. It is the fourth-largest planet by diameter and the third-largest by mass. Among the gaseous planets in the solar system, Neptune is the most dense. Neptune is 17 times the mass of Earth and is slightly more massive than its near-twin Uranus, which is 15 times the mass of Earth but not as dense. On average, Neptune orbits the Sun at a distance of 30.1 AU, approximately 30 times the Earth-Sun distance. Named for the Roman god of the sea, its astronomical symbol is a stylised version of the god Neptune's trident.",
+    wikipediaPage: "Neptune"
+  },
+
+  uranus: {
+    name: "Uranus",
+    videoId: "G7f1nuwRq_E",
+    price: 7000000,
+    distance: 1898000000,
+    blurb: "Uranus is the seventh planet from the Sun. It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System. Uranus is similar in composition to Neptune, and both are of different chemical composition than the larger gas giants Jupiter and Saturn. For this reason, astronomers sometimes place them in a separate category called 'ice giants'. Uranus's atmosphere, although similar to Jupiter's and Saturn's in its primary composition of hydrogen and helium, contains more 'ices' such as water, ammonia, and methane, along with traces of hydrocarbons.",
+    wikipediaPage: "Uranus"
+  },
+
+  pluto: {
+    name: "Pluto",
+    videoId: "e98L6mftkNU",
+    price: 8000000,
+    distance: 2928000000,
+    blurb: "Pluto, formal designation 134340 Pluto, is the second-most-massive known dwarf planet in the Solar System (after Eris) and the tenth-most-massive body observed directly orbiting the Sun. Originally classified as the ninth planet from the Sun, Pluto was recategorized as a dwarf planet and plutoid owing to the discovery that it is only one of several large bodies within the Kuiper belt.",
+    wikipediaPage: "Pluto"
+  },
+
+  moon: {
+    name: "Moon",
+    videoId: "2iSZMv64wuU",
+    price: 500000,
+    distance: 252562,
+    blurb: "The Moon is the only natural satellite of the Earth, having 27% the diameter and 60% the density of Earth, resulting in 1⁄81 its mass. The Moon is the second densest satellite after Io, a satellite of Jupiter.",
+    wikipediaPage: "Moon"
+  },
+
+  nibiru: {
+    name: "Nibiru",
+    videoId: "m-ZG2nyUXiY",
+    price: 999999999,
+    distance: 42,
+    blurb: "Nibiru, which is said to have occupied the passageways of heaven and earth, because everyone above and below asks Nibiru if they cannot find the passage. Nibiru is Marduk's star which the gods in heaven caused to be visible. Nibiru stands as a post at the turning point. The others say of Nibiru the post: 'The one who crosses the middle of the sea (Tiamat) without calm, may his name be Nibiru, for he takes up the center of it'. The path of the stars of the sky should be kept unchanged.",
+    wikipediaPage: "Nibiru_(Babylonian_astronomy)"
+  }
+}
